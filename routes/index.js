69f51b18e6d7f0e83b420143b7a7eb8f4c955031
Shimil <shimiltharayil007@gmail.com>
2022-11-16 11:13:28 +0530

    layout added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  let products = [
+    {
+      name: "Iphone 14",
+      description: "Apple Product",
+      image:
+        " https://brain-images-ssl.cdn.dixons.com/9/1/10215619/l_10215619.jpg",
+    },
+    {
+      name: "Iphone 13",
+      description: "Apple Product",
+      image:
+        "https://brain-images-ssl.cdn.dixons.com/9/1/10215619/l_10215619.jpg ",
+    },
+    {
+      name: "Iphone 12",
+      description: "Apple Product",
+      image:
+        " https://brain-images-ssl.cdn.dixons.com/9/1/10215619/l_10215619.jpg",
+    },
+    {
+      name: "Iphone 11",
+      description: "Apple Product",
+      image:
+        " https://brain-images-ssl.cdn.dixons.com/9/1/10215619/l_10215619.jpg",
+    },
+  ];
+  res.render("index", { products });
 });
 
 module.exports = router;
